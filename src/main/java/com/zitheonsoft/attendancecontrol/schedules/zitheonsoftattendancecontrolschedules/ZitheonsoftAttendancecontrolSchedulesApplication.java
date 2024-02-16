52006301f9fb3fb0c90caefefd838a4b0629acdc
AlexGarcia53 <alex.garcia228721@potros.itson.edu.mx>
2024-02-15 19:38:24 -0700

    first commit

---
+++ src/main/java/com/zitheonsoft/attendancecontrol/schedules/zitheonsoftattendancecontrolschedules/ZitheonsoftAttendancecontrolSchedulesApplication.java
@@ -0,0 +1,13 @@
+package com.zitheonsoft.attendancecontrol.schedules.zitheonsoftattendancecontrolschedules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZitheonsoftAttendancecontrolSchedulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZitheonsoftAttendancecontrolSchedulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zitheonsoft/attendancecontrol/schedules/zitheonsoftattendancecontrolschedules/ZitheonsoftAttendancecontrolSchedulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zitheonsoft.attendancecontrol.schedules.zitheonsoftattendancecontrolschedules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZitheonsoftAttendancecontrolSchedulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
